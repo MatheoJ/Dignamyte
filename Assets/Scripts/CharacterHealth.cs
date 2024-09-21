3f287dc50e1a1e908998d8c5c51339b9583b9f4c
JuliePec <julie.pecuch@umontreal.ca>
2024-09-21 12:02:36 -0400

    Health

---
+++ Assets/Scripts/CharacterHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterHealth : MonoBehaviour
+{
+
+    public int health = 1;
+
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ennemi" || collision.gameObject.tag == "Bomb" )
+        {
+            health = 0;
+        }
+    }
+
+}
