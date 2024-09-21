e19107706cae34c9e2f6a143cb3c5f2eac26765c
JuliePec <julie.pecuch@umontreal.ca>
2024-09-21 12:02:36 -0400

    Health

---
+++ Assets/Scripts/PoweupEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PoweupEffect : ScriptableObject
+{
+    public abstract void Apply(GameObject target);
+
+
+
+
+
+}
