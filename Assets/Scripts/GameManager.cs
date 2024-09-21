127c071ee5ead3acf6bab100f8a9dee26d67e9ff
MatheoJ <matheo.joseph.01@gmail.com>
2024-09-21 14:09:11 -0400

    Start gameManager  and UI

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private InGameUIManager inGameUIManager;
+    
+    private int killCount = 0;
+    
+    private float timeSinceGameStart;
+    
+    private bool areEnemiesStunned = false; 
+    
+    private float stuntTime = 5.0f;
+    
+    private float timeSinceStuntStart = 0.0f;
+    
+    public bool AreEnemiesStunned
+    {
+        get
+        {
+            return areEnemiesStunned;
+        }
+        set
+        {
+            areEnemiesStunned = value;
+            if(areEnemiesStunned)
+            {
+                timeSinceStuntStart = Time.time;
+            }
+        }
+    }
+    
+    void Start()
+    {
+        timeSinceGameStart = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateEnemyStunStatus();
+    }
+    
+    public void IncrementKillCount()
+    {
+        killCount++;
+        inGameUIManager.UpdateKillCount(killCount);
+    }
+    
+    public void setStuntTime(float time)
+    {
+        stuntTime = time;
+    }
+    
+    private void UpdateEnemyStunStatus()
+    {
+        if(areEnemiesStunned)
+        {
+            if(Time.time - timeSinceStuntStart > stuntTime)
+            {
+                areEnemiesStunned = false;
+            }
+        }
+    }
+}
