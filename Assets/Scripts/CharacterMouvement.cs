c6d7673757c70196fb41475e498849fe74ccb6c9
JuliePec <julie.pecuch@umontreal.ca>
2024-09-21 11:14:18 -0400

    Character

--- Assets/Scripts/CharacterMouvement.cs
+++ Assets/Scripts/CharacterMouvement.cs
@@ -1,18 +1,60 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterMouvement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _turnSpeed = 360;
+    private Vector3 _input;
+
+    private void Update()
     {
-        
+        GatherInput();
+        Look();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
     }
+
+
+    void Look()
+    {
+
+        if(_input != Vector3.zero) 
+        {
+          
+            //Iso up and down
+           //var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+
+            //var skewedInput = matrix.MultiplyPoint3x4(_input);
+
+
+            //Normal uo and down
+            var relative = (transform.position + _input) - transform.position;
+
+            //Iso up and down
+            //var relative = (transform.position + skewedInput) - transform.position;
+
+            var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
+        }
+
+;    }
+    void GatherInput()
+    {
+        _input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+
+    private void Move()
+    {
+        _rb.MovePosition(transform.position + (transform.forward * _input.magnitude) * _speed * Time.deltaTime);
+
+    }
+
 }
