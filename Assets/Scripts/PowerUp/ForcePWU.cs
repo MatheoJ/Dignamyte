dc83bb7679e8add884b4350a5a94d32ac2c17962
JuliePec <julie.pecuch@umontreal.ca>
2024-09-21 14:25:08 -0400

    PWU

---
+++ Assets/Scripts/PowerUp/ForcePWU.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Powerups/Force")]
+
+public class ForcePWU : PoweupEffect
+{
+    public override void Apply(GameObject target)
+    {
+
+        target.GetComponent<CharacterHealth>().invincible = true;
+    }
+
+}
---
+++ Assets/Scripts/PowerUp/RangePWU.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Powerups/Range")]
+
+public class RangePWU : PoweupEffect
+{
+
+
+    public override void Apply(GameObject target)
+    {
+        
+       // target.GetComponent<>()._speed += amount;
+    }
+}
