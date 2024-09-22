bdfde3f0ba608318aec2db3111c31b30271bac17
JuliePec <julie.pecuch@umontreal.ca>
2024-09-21 20:29:00 -0400

    text

---
+++ Assets/Scripts/ScoreText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreText : MonoBehaviour
+{
+    public Text score;
+
+    int scoreCount;
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreCount = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>().killCount;
+        score.text = "Kill Count : " + scoreCount;
+    }
+}
